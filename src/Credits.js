a2281be6643ceba271b155df315ce1ec80dfe4b8
SpendBCH <SpendBCH@SpendBCH.io>
2018-08-18 10:54:13 -0700

    Update bitbox link

--- src/Credits.js
+++ src/Credits.js
@@ -4,7 +4,7 @@ class Credits extends Component {
   render() {
     return (
       <div>
-        Created by <a className={this.props.classes.credits} href='https://twitter.com/SpendBCH_io'>@SpendBCH_io</a> and <a className={this.props.classes.credits} href='https://twitter.com/cgcardona'>@cgcardona</a>. Made with <a className={this.props.classes.credits} href='https://www.bitbox.earth/'>BITBOX</a>
+        Created by <a className={this.props.classes.credits} href='https://twitter.com/SpendBCH_io'>@SpendBCH_io</a> and <a className={this.props.classes.credits} href='https://twitter.com/cgcardona'>@cgcardona</a>. Powered by <a className={this.props.classes.credits} href='https://developer.bitcoin.com/bitbox.html'>BITBOX</a>
       </div>
     );
   }
